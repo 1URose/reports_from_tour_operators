b631e93ed9fe7368272d6b2127bf48c57cc1951d
Ermakov Yaroslav <pengven357@gmail.com>
2026-06-15 16:49:25 +0300

    updated

--- main.py
+++ main.py
@@ -119,8 +119,8 @@ def print_detailed_tables(counters, unclassified, group_totals, overall_total):
             continue
         print(group_name)
         for key, value in sorted(group_counts.items(), key=lambda x: (-x[1], x[0])):
-            print(f"- {key} - {value} шт")
-            # print(f"{key}\t{value}")
+            # print(f"- {key} - {value} шт")
+            print(f"{key}\t{value}")
         print()
 
     if unclassified:
@@ -504,7 +504,8 @@ def get_dynamic_groups():
                 "not found in tour"
             ],
             "tour expired": [
-                "while calling concretize: while actualizing tour: tour expired"
+                "error to prepare booking: fail to prebook tour: while calling concretize: while actualizing tour: tour expired",
+                "fail to prebook tour: while calling concretize: while selecting and concretizing route: tour expired"
             ],
 
         }),
@@ -703,13 +704,11 @@ def get_dynamic_groups():
             ],
 
             "tour expired": [
-                # "while calling concretize: while actualizing tour: tour expired",
             #     "while actualizing tour: tour expired",
             #     "while changing route: tour expired",
             #     "by LT: One or more errors occurred. (Error from supplier)",
             #     "by LT: 176242988882209831 Code - 0",
             #     "Произошла ошибка при выполнении запроса актуализации",
-            #     "while selecting and concretizing route: tour expired",
             ],
         }),
 
@@ -778,6 +777,11 @@ def get_dynamic_groups():
                 'by LT: //meta.online-express.ru/api/v2/accommodations/search?checkInDate=2026-06-01&checkOutDate=2026-06-18&currency=RUB&hotelId=224669&rooms%5B0%5D%5Badults%5D=2&rooms%5B0%5D%5Bchildren%5D=0": Service Unavailable',
                 "tour expired: by LT: expected element type <HotelPricingResponse2> but have <html>",
                 'tour expired: by LT: short_rus="2 места", rus="2 места", short_eng="", eng=""',
+                "tour expired: by LT: code - ERROR_EXCEPTION, description - Value cannot be null. (Parameter 'source')",
+                "tour expired: by LT: code - ERROR_EXCEPTION, description - AviaPricing error",
+                "by LT: code - ERROR_EXCEPTION, description - Check availability fail.",
+                "tour expired: by LT: | Code - 0",
+                "tour expired: by LT: code = Unavailable desc = upstream connect error or disconnect/reset before headers. reset reason",
             ],
             "response error" : [
                 "tour expired: by LT: response error"
@@ -790,6 +794,9 @@ def get_dynamic_groups():
             ],
             "Не удалось подтвержить наличии свободных мест на перелет" : [
                 "tour expired: by LT: К сожалению, мы не получили подтверждение о наличии свободных мест на этом перелёте."
+            ],
+            "tour expired by LT": [
+                "tour expired: by LT: failed to call actualize: offer expired",
             ],
             "bundle ID mismatch": [
                 "while calling create booking: create booking error: by LT:", # Временная замена, так как не можем нормально распарсить  {"booking":null,"request_info":{"code":0,"error":{"cause":{"cause":null,"code":500,"detalization":{"critical":false,"http_status":0,"supplier_code":0},"message":"failed to create booking in meta middleware: failed to create booking in validation middleware: error finding or creating booking: handler failed to find or create booking: failed to find or create booking in logging middleware: failed to build booking: failed to load bundles: bundle ID mismatch: expected 01|230402Ic5vomOK::8eOnl9RT7O|310401toU9_O9q::MtLNoUW3::zEIsJwo9ff|090404B1JLvMZd::IgrXaK55yO, got 01|230402Ic5vomOK::8eOnl9RT7O|310401toU9_O9q::MtLNoUW3::j4dGgSsmLf|090404B1JLvMZd::IgrXaK55yO","type":"ERROR_TYPE_INTERNAL"},"code":500,"detalization":{"critical":false,"http_status":0,"supplier_code":0},"message":"Internal server error (from middleware)","type":"ERROR_TYPE_INTERNAL"},"request_id":"2a22ebef-f6fb-4b5a-a98c-8502a8c76574","warnings":[]}}
